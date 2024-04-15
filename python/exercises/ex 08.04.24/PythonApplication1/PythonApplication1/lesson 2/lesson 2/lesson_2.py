4470252063f85d4dd61556f147b037e91dbf22be
yairelitz <yair99eli@gmail.com>
2024-04-15 14:22:54 +0300

    new exercises

--- python/exercises/ex 08.04.24/PythonApplication1/PythonApplication1/lesson 2/lesson 2/lesson_2.py
+++ python/exercises/ex 08.04.24/PythonApplication1/PythonApplication1/lesson 2/lesson 2/lesson_2.py
@@ -1,89 +1,89 @@
-import random
-r=ord('a')
-s=""
-c=chr(random.randint(97,106))
-s=s+c
-c=chr(random.randint(97,106))
-s=s+c
-c=chr(random.randint(97,106))
-s=s+c
-c=chr(random.randint(97,106))
-s=s+c
-print(s)
+# import random
+# r=ord('a')
+# s=""
+# c=chr(random.randint(97,106))
+# s=s+c
+# c=chr(random.randint(97,106))
+# s=s+c
+# c=chr(random.randint(97,106))
+# s=s+c
+# c=chr(random.randint(97,106))
+# s=s+c
+# print(s)
 
 
-m=[
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0]
-    ]
-
-
-
-
-
-
-def S1InS2(s1, s2):
-    if(len(s1)>len(s2)):
-        return False
-    for i in range(len(s1)):
-        for k in range(len(s2)):
-            if(s1[i]==s2[k]):
-                break
-        if(s1[i]!=s2[k]):
-            return False
-    return True
-print(S1InS2("cea", "casv"))    
-print(S1InS2("cxea", "abcdef"))          
-print(S1InS2("cjea", "abcdef"))          
-print(S1InS2("cea", "cafehb"))    
+# m=[
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0]
+#     ]
 
 
 
 
-def S1InS2(s1, s2):
-    if(len(s1)>len(s2)):
-        return False
-    i=0
-    while(i<len(s1)):
-        k=0
-        while(k<len(s2)):
-            if(s1[i]==s2[k]):
-                break
-            k+=1
-        if(k==len(s2)):
-            return False
-        i+=1
-    return True
-print(S1InS2("cxea", "abcdef"))          
-print(S1InS2("cjea", "abcdef"))          
-print(S1InS2("ceav", "cae"))           
+
+
+# def S1InS2(s1, s2):
+#     if(len(s1)>len(s2)):
+#         return False
+#     for i in range(len(s1)):
+#         for k in range(len(s2)):
+#             if(s1[i]==s2[k]):
+#                 break
+#         if(s1[i]!=s2[k]):
+#             return False
+#     return True
+# print(S1InS2("cea", "casv"))    
+# print(S1InS2("cxea", "abcdef"))          
+# print(S1InS2("cjea", "abcdef"))          
+# print(S1InS2("cea", "cafehb"))    
 
 
 
-def CmpString(s1, s2):
-    l=len(s1)
-    if(len(s1)>len(s2)):
-        l=len(s2)
-    for i in range(l):
-        if(s1[i] != s2[i]):
-           return ord(s1[i]) - ord(s2[i])
-    return len(s1)-len(s2)
+
+# def S1InS2(s1, s2):
+#     if(len(s1)>len(s2)):
+#         return False
+#     i=0
+#     while(i<len(s1)):
+#         k=0
+#         while(k<len(s2)):
+#             if(s1[i]==s2[k]):
+#                 break
+#             k+=1
+#         if(k==len(s2)):
+#             return False
+#         i+=1
+#     return True
+# print(S1InS2("cxea", "abcdef"))          
+# print(S1InS2("cjea", "abcdef"))          
+# print(S1InS2("ceav", "cae"))           
+
+
+
+# def CmpString(s1, s2):
+#     l=len(s1)
+#     if(len(s1)>len(s2)):
+#         l=len(s2)
+#     for i in range(l):
+#         if(s1[i] != s2[i]):
+#            return ord(s1[i]) - ord(s2[i])
+#     return len(s1)-len(s2)
    
 
-print(CmpString("aaa", "bbb"))        
-print(CmpString("ccc", "bbb"))        
-print(CmpString("aaa", "abb"))        
-print(CmpString("axa", "aaaa"))        
-print(CmpString("aaaa", "aaa"))        
-print(CmpString("aaa", "aaaaaaaa"))        
+# print(CmpString("aaa", "bbb"))        
+# print(CmpString("ccc", "bbb"))        
+# print(CmpString("aaa", "abb"))        
+# print(CmpString("axa", "aaaa"))        
+# print(CmpString("aaaa", "aaa"))        
+# print(CmpString("aaa", "aaaaaaaa"))        
            
 
 
@@ -114,122 +114,122 @@ print(CmpString("aaa", "aaaa"))
 print(CmpString("aaaa", "aaa"))        
            
 
-import random
-def Show(a):
-    for r in range(len(a)):
-        for c in range(len(a)):
-            print(a[r][c], end=" ")
-        print()
+# import random
+# def Show(a):
+#     for r in range(len(a)):
+#         for c in range(len(a)):
+#             print(a[r][c], end=" ")
+#         print()
            
-r=ord('a')
-s=""
-for i in range(100):
-    c=chr(random.randint(97,106))
-    s=s+c
-print(s)
-m=[
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0]
-    ]
-for i in range(len(s)-1):
-    sh=ord(s[i])-97
-    a=ord(s[i+1])-97
-    m[sh][a]+=1
-Show(m)    
+# r=ord('a')
+# s=""
+# for i in range(100):
+#     c=chr(random.randint(97,106))
+#     s=s+c
+# print(s)
+# m=[
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0]
+#     ]
+# for i in range(len(s)-1):
+#     sh=ord(s[i])-97
+#     a=ord(s[i+1])-97
+#     m[sh][a]+=1
+# Show(m)    
 
 
-import random
-r=ord('a')
-s=""
-c=chr(random.randint(97,106))
-s=s+c
-c=chr(random.randint(97,106))
-s=s+c
-c=chr(random.randint(97,106))
-s=s+c
-c=chr(random.randint(97,106))
-s=s+c
-print(s)
+# import random
+# r=ord('a')
+# s=""
+# c=chr(random.randint(97,106))
+# s=s+c
+# c=chr(random.randint(97,106))
+# s=s+c
+# c=chr(random.randint(97,106))
+# s=s+c
+# c=chr(random.randint(97,106))
+# s=s+c
+# print(s)
 
  
-m=[
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0]
-    ]
+# m=[
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0],
+#     [0,0,0,0,0,0,0,0,0,0]
+#     ]
 
 
 
 
 
 
-def S1InS2(s1, s2):
-    if(len(s1)>len(s2)):
-        return False
-    for i in range(len(s1)):
-        for k in range(len(s2)):
-            if(s1[i]==s2[k]):
-                break
-        if(s1[i]!=s2[k]):
-            return False
-    return True
-print(S1InS2("cea", "casv"))    
-print(S1InS2("cxea", "abcdef"))          
-print(S1InS2("cjea", "abcdef"))          
-print(S1InS2("cea", "cafehb"))    
+# def S1InS2(s1, s2):
+#     if(len(s1)>len(s2)):
+#         return False
+#     for i in range(len(s1)):
+#         for k in range(len(s2)):
+#             if(s1[i]==s2[k]):
+#                 break
+#         if(s1[i]!=s2[k]):
+#             return False
+#     return True
+# print(S1InS2("cea", "casv"))    
+# print(S1InS2("cxea", "abcdef"))          
+# print(S1InS2("cjea", "abcdef"))          
+# print(S1InS2("cea", "cafehb"))    
 
 
 
 
-def S1InS2(s1, s2):
-    if(len(s1)>len(s2)):
-        return False
-    i=0
-    while(i<len(s1)):
-        k=0
-        while(k<len(s2)):
-            if(s1[i]==s2[k]):
-                break
-            k+=1
-        if(k==len(s2)):
-            return False
-        i+=1
-    return True
-print(S1InS2("cxea", "abcdef"))          
-print(S1InS2("cjea", "abcdef"))          
-print(S1InS2("ceav", "cae"))           
+# def S1InS2(s1, s2):
+#     if(len(s1)>len(s2)):
+#         return False
+#     i=0
+#     while(i<len(s1)):
+#         k=0
+#         while(k<len(s2)):
+#             if(s1[i]==s2[k]):
+#                 break
+#             k+=1
+#         if(k==len(s2)):
+#             return False
+#         i+=1
+#     return True
+# print(S1InS2("cxea", "abcdef"))          
+# print(S1InS2("cjea", "abcdef"))          
+# print(S1InS2("ceav", "cae"))           
 
-def CmpString(s1, s2):
-    l=len(s1)
-    if(len(s1)>len(s2)):
-        l=len(s2)
-    for i in range(l):
-        if(s1[i] != s2[i]):
-           return ord(s1[i]) - ord(s2[i])
-    return len(s1)-len(s2)
+# def CmpString(s1, s2):
+#     l=len(s1)
+#     if(len(s1)>len(s2)):
+#         l=len(s2)
+#     for i in range(l):
+#         if(s1[i] != s2[i]):
+#            return ord(s1[i]) - ord(s2[i])
+#     return len(s1)-len(s2)
    
 
-print(CmpString("aaa", "bbb"))        
-print(CmpString("ccc", "bbb"))        
-print(CmpString("aaa", "abb"))        
-print(CmpString("axa", "aaaa"))        
-print(CmpString("aaaa", "aaa"))        
-print(CmpString("aaa", "aaaaaaaa"))        
+# print(CmpString("aaa", "bbb"))        
+# print(CmpString("ccc", "bbb"))        
+# print(CmpString("aaa", "abb"))        
+# print(CmpString("axa", "aaaa"))        
+# print(CmpString("aaaa", "aaa"))        
+# print(CmpString("aaa", "aaaaaaaa"))        
            
 
 
@@ -237,24 +237,24 @@ print(CmpString("aaa", "aaaaaaaa"))
 
 
 
-def CmpString(s1, s2):
-    l=len(s1)
-    if(len(s1)>len(s2)):
-        l=len(s2)
-    for i in range(l):
-        if(ord(s1[i]) < ord(s2[i])):
-           return -1
-        else:
-           if(ord(s1[i]) > ord(s2[i])):
-               return 1
-    if(len(s1)==len(s2)):
-        return 0
-    else:
-        return len(s1)-len(s2)
+# def CmpString(s1, s2):
+#     l=len(s1)
+#     if(len(s1)>len(s2)):
+#         l=len(s2)
+#     for i in range(l):
+#         if(ord(s1[i]) < ord(s2[i])):
+#            return -1
+#         else:
+#            if(ord(s1[i]) > ord(s2[i])):
+#                return 1
+#     if(len(s1)==len(s2)):
+#         return 0
+#     else:
+#         return len(s1)-len(s2)
    
 
-print(CmpString("aaa", "bbb"))        
-print(CmpString("ccc", "bbb"))        
-print(CmpString("aaa", "abb"))        
-print(CmpString("aaa", "aaaa"))        
-print(CmpString("aaaa", "aaa"))   
+# print(CmpString("aaa", "bbb"))        
+# print(CmpString("ccc", "bbb"))        
+# print(CmpString("aaa", "abb"))        
+# print(CmpString("aaa", "aaaa"))        
+# print(CmpString("aaaa", "aaa"))   
--- python/exercises/ex 11.04.24/python lesson 11.04.24/python lesson 11.04.24/python_lesson_11.04.24.py
+++ python/exercises/ex 11.04.24/python lesson 11.04.24/python lesson 11.04.24/python_lesson_11.04.24.py
@@ -1,198 +1,198 @@
-a=[
-    [3,5,7,6,9],
-    [3,5,4,1,2,4,4,5],
-    [7,4,5,6,7,3],
-    [1,2,3],
-]
-for r in range(len(a)):
-    for c in range(len(a[r])):
-        print(a[r][c], end=" ")
-    print()
+# a=[
+#     [3,5,7,6,9],
+#     [3,5,4,1,2,4,4,5],
+#     [7,4,5,6,7,3],
+#     [1,2,3],
+# ]
+# for r in range(len(a)):
+#     for c in range(len(a[r])):
+#         print(a[r][c], end=" ")
+#     print()
        
-'''
-3 5 7 6 9      
-3 5 4 1 2 4 4 5
-7 4 5 6 7 3
-1 2 3
-'''
+# '''
+# 3 5 7 6 9      
+# 3 5 4 1 2 4 4 5
+# 7 4 5 6 7 3
+# 1 2 3
+# '''
 
 
-a=[45,102,674,19,876,45]
-         #a=[102,674,19,876,45,45]     Changes place in a left-rotational manner
-temp=a[0]
-for i in range(1, len(a)):
-    a[i-1]=a[i]
-         #a[len(a)-1]=temp    
-a[i]=temp    
-print(a)    
-
-
-
-
-a=[-456,-102,-67834,-19,-876,-45]
-        #   a[-19]     the largest number
-max=a[0]
-for i in range(len(a)):
-    if(max<a[i]):
-        max=a[i]
-print(max)    
+# a=[45,102,674,19,876,45]
+#          #a=[102,674,19,876,45,45]     Changes place in a left-rotational manner
+# temp=a[0]
+# for i in range(1, len(a)):
+#     a[i-1]=a[i]
+#          #a[len(a)-1]=temp    
+# a[i]=temp    
+# print(a)    
 
 
 
 
-
-
-a=[456,102,67834,1009,876,45]
-        #a=[6,2,8,9,8,5]        The largest number in any index 
-for i in range(len(a)):
-    max=0
-    while(a[i]>0):
-        if(max<a[i]%10):
-            max=a[i]%10
-        a[i]=a[i]//10
-    a[i]=max
-print(a)    
-
-
-
-
-a=[456,102,67834,1009,876,45]
-#a=[15,3,28,10,21,9]           The sum of the digits in each index
-for i in range(len(a)):
-    sum=0
-    while(a[i]>0):
-        sum=sum+a[i]%10
-        a[i]=a[i]//10
-    a[i]=sum
-print(a)    
+# a=[-456,-102,-67834,-19,-876,-45]
+#         #   a[-19]     the largest number
+# max=a[0]
+# for i in range(len(a)):
+#     if(max<a[i]):
+#         max=a[i]
+# print(max)    
 
 
 
 
 
-a=[456,102,67834,1009,876,45]
-#a=[4,1,6,9,8,4]              Only the left digit
-for i in range(len(a)):
-    while(a[i]>9):
-        a[i]=a[i]//10
-print(a)    
+
+# a=[456,102,67834,1009,876,45]
+#         #a=[6,2,8,9,8,5]        The largest number in any index 
+# for i in range(len(a)):
+#     max=0
+#     while(a[i]>0):
+#         if(max<a[i]%10):
+#             max=a[i]%10
+#         a[i]=a[i]//10
+#     a[i]=max
+# print(a)    
 
 
 
 
-a=[456,12,67834,9,876,45]
-#a=[3,2,5,1,3,2]           The number of digits in each index
-for i in range(len(a)):
-    c=0
-    while(a[i]>0):
-        c+=1
-        a[i]=a[i]//10
-    a[i]=c
-print(a)    
+# a=[456,102,67834,1009,876,45]
+# #a=[15,3,28,10,21,9]           The sum of the digits in each index
+# for i in range(len(a)):
+#     sum=0
+#     while(a[i]>0):
+#         sum=sum+a[i]%10
+#         a[i]=a[i]//10
+#     a[i]=sum
+# print(a)    
 
 
 
-def IntToString(n):
-    s=""
-    while(n>0):
-        t=chr((n%10)+48)
-        s=t+s
-        n=n//10
-    return s
+
+
+# a=[456,102,67834,1009,876,45]
+# #a=[4,1,6,9,8,4]              Only the left digit
+# for i in range(len(a)):
+#     while(a[i]>9):
+#         a[i]=a[i]//10
+# print(a)    
+
+
+
+
+# a=[456,12,67834,9,876,45]
+# #a=[3,2,5,1,3,2]           The number of digits in each index
+# for i in range(len(a)):
+#     c=0
+#     while(a[i]>0):
+#         c+=1
+#         a[i]=a[i]//10
+#     a[i]=c
+# print(a)    
+
+
+
+# def IntToString(n):
+#     s=""
+#     while(n>0):
+#         t=chr((n%10)+48)
+#         s=t+s
+#         n=n//10
+#     return s
    
-y=6834
-x=IntToString(y)
-x=x+"fdhf"
-print(x)
+# y=6834
+# x=IntToString(y)
+# x=x+"fdhf"
+# print(x)
 
-x=6834
-y=str(x)
-y=y+"***"
-print(y)
-
-
-
-def StringToInt(s): #   s="6824"
-    num=0
-    ord0=ord('0')   #   48
-    for i in range(len(s)):
-        num = num*10 + ord(s[i])-ord0
-    return num
-qqq=input("Enter a number : ")
-qqq="4561"
-n = StringToInt(qqq)
-n=n+1
-print(n)
-
-#   3 6 2   ====>   362
-#   0*10 + 3    3
-#   3*10 + 6    36
-#   36*10+ 2    362
-
-
-r=9
-t=r+1
-while(r>0):
-    for s in range(r, 0, -1):
-        print("*", end=" ")
-    s=r
-    while(s<t):
-        print(r, end=" ")
-        s+=1
-    # for s in range(r, t, 1):    
-    #     print(r, end=" ")
-    print()
-    r-=1
-
-'''
-*****5
-****44
-***333
-**2222
-*11111
-
-'''
+# x=6834
+# y=str(x)
+# y=y+"***"
+# print(y)
 
 
 
-def Show(n):
-    for r in range(n):    #   (0, n, 1)
-        for c in range(-1, r, 1):
-            print("*", end=" ")
-        print()
-Show(4)
-Show(5)
+# def StringToInt(s): #   s="6824"
+#     num=0
+#     ord0=ord('0')   #   48
+#     for i in range(len(s)):
+#         num = num*10 + ord(s[i])-ord0
+#     return num
+# qqq=input("Enter a number : ")
+# qqq="4561"
+# n = StringToInt(qqq)
+# n=n+1
+# print(n)
+
+# #   3 6 2   ====>   362
+# #   0*10 + 3    3
+# #   3*10 + 6    36
+# #   36*10+ 2    362
 
 
-def Mul(a, b):
-    if(a<b):
-        r=0
-        while(a>0):
-            r=r+b
-            a-=1
-    else:
-        r=0
-        while(b>0):
-            r=r+a
-            b-=1
-    return r
+# r=9
+# t=r+1
+# while(r>0):
+#     for s in range(r, 0, -1):
+#         print("*", end=" ")
+#     s=r
+#     while(s<t):
+#         print(r, end=" ")
+#         s+=1
+#     # for s in range(r, t, 1):    
+#     #     print(r, end=" ")
+#     print()
+#     r-=1
 
-print(Mul(5, 6))
-print(Mul(5, 6))
+# '''
+# *****5
+# ****44
+# ***333
+# **2222
+# *11111
+
+# '''
 
 
-d=0
-r=1
-while(r<5):
-    d=d+r
-    print(d, r)
-    r+=1
-print((d+r), r)
-#   d=0     1   3   6   10  
-#   r=1     2   3   4   5  
 
-#                   1   1
-#                   3   2
-#                   3   2
-#                   10  4
-#                   10  5
+# def Show(n):
+#     for r in range(n):    #   (0, n, 1)
+#         for c in range(-1, r, 1):
+#             print("*", end=" ")
+#         print()
+# Show(4)
+# Show(5)
+
+
+# def Mul(a, b):
+#     if(a<b):
+#         r=0
+#         while(a>0):
+#             r=r+b
+#             a-=1
+#     else:
+#         r=0
+#         while(b>0):
+#             r=r+a
+#             b-=1
+#     return r
+
+# print(Mul(5, 6))
+# print(Mul(5, 6))
+
+
+# d=0
+# r=1
+# while(r<5):
+#     d=d+r
+#     print(d, r)
+#     r+=1
+# print((d+r), r)
+# #   d=0     1   3   6   10  
+# #   r=1     2   3   4   5  
+
+# #                   1   1
+# #                   3   2
+# #                   3   2
+# #                   10  4
+# #                   10  5
